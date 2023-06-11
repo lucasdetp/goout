e6cf2965f0ef7b4bb6c76fe3bb49b746c4ab55ca
lucasdetp <detplucas@mail.com>
2023-06-11 15:06:53 +0200

    feat: css

--- resources/views/pages/a-propos.blade.php
+++ resources/views/pages/a-propos.blade.php
@@ -24,59 +24,61 @@
         </div>
 
     </div>
-    <div class="objectifs left" id="objectifs">
-        <div class="title">
-            <!-- <img src="" alt="picto"> -->
-            <h1>Objectifs</h1>
+    <div class="all-propos">
+        <div class="objectifs left" id="objectifs">
+            <div class="title">
+                <img src="{{ URL::asset('images/objectifs.png') }}" alt="picto">
+                <h1>Objectifs</h1>
+            </div>
+            <div class="desc-propos left-desc">
+                <p>Notre mission chez Go Out ! est de <span>connecter les jeunes adultes</span> passionnés de sorties et de divertissements en leur offrant un moyen simple et convivial de découvrir des événements excitants autour d'eux. Nous voulons créer une communauté dynamique où chacun peut trouver des expériences uniques qui correspondent à ses goûts.</p>
+            </div>
         </div>
-        <div class="desc-propos left-desc">
-            <p>Notre missions chez Go Out ! est de <span>connecter les jeunes adultes</span> passionées de sorties et de divertissements en leur offrant un moyen simple et convivial de découvrir des événements excitants autour d'eux. Nous voulons créer une communauté dynamique où chacun peut trouver des expériences uniques qui correspondent à ses goûts.</p>
+        <div class="valeurs right" id="valeurs">
+            <div class="title">
+                <h1>Valeurs</h1>
+                <img src="{{ URL::asset('images/valeurs.png') }}" alt="picto">
+            </div>
+            <div class="desc-propos right-desc">
+                <p>Chez Go Out ! Nous croyons en <span>l'inclusivité, la bienveillance et le partage</span>. Nous encourageons nos utilisateurs à découvrir de nouvelles activités, à se rencontrer et à partager des expériences enrichissantes. Nous accordons une grande importance à la sécurité de nos utilisateurs et nous nous efforçons de créer un environnement amical et sécurié pour tous.</p>
+            </div>
         </div>
-    </div>
-    <div class="valeurs right" id="valeurs">
-        <div class="title">
-            <!-- <img src="" alt="picto"> -->
-            <h1>Valeurs</h1>
+        <div class="historiques left" id="historiques">
+            <div class="title">
+                <img src="{{ URL::asset('images/historique.png') }}" alt="picto">
+                <h1>Historiques</h1>
+            </div>
+            <div class="desc-propos left-desc">
+                <p>Go Out ! est né de la passion commune de notre équipe pour les sorties et les événements. Nous avons réalisé qu'il était parfois <span>difficiles de trouver des soirées et des activités intéressantes,</span> surtout lorsque l'on est nouveau dans une ville ou que l'on cherche à diversifier ses expériences. C'est ainsi que nous avons décidé de créer Go Out ! pour faciliter la découverte et l'organisation d'événements.</p>
+            </div>
         </div>
-        <div class="desc-propos right-desc">
-            <p>Chez Go Out ! Nous croyons en <span>l'inclusivité, la bienveillance et le partage</span>. Nous encourageons nos utilisateurs à découvrir de nouvelles activités, à se rencontrer et à partager des expériences enrichissantes. Nous accordons une grande importance à la sécurité de nos utilisateurs et nous nous efforçons de créer un environnement amical et sécurié pour tous.</p>
+        <div class="equipe right" id="equipe">
+            <div class="title">
+                <h1>Équipe</h1>
+                <img src="{{ URL::asset('images/equipe.png') }}" alt="picto">
+            </div>
+            <div class="desc-propos right-desc">
+                <!-- <img src="{{ URL::asset('images/notre-equipe.png') }}" alt="notre équipe"> -->
+                <p>Notre équipe de <span>passionés</span> travaille sans relâche pour prendre Go Out ! plus excitant et plus complet chaque jour. Nous sommes des amateurs de sorties et nous comprenons les besoins et les attentes de notre communauté. Nous mettons tous en oeuvre pour offrir une expérience utilisateur exceptionnelle et pour réponde aux besoin de notre communauté grandissante.</p>
+            </div>
         </div>
-    </div>
-    <div class="historiques left" id="historiques">
-        <div class="title">
-            <!-- <img src="" alt="picto"> -->
-            <h1>Historiques</h1>
+        <div class="engagements left" id="engagements">
+            <div class="title">
+                <img src="{{ URL::asset('images/engagements.png') }}" alt="picto">
+                <h1>Engagements</h1>
+            </div>
+            <div class="desc-propos left-desc">
+                <p>Chez Go Out ! nous nous engageons à créer une <span>communauté active, engagée et respectueuse</span>. Nous encourageons nos utilisateurs à interagir, à se rencontrer et à partager des moments mémorables. La sécurité de nos utilisateurs est primordiale, c'est pourquoi nous avons mis en place des mesures de protection et de confidentialité pour garantir une expérience sûre et agréable.</p>
+            </div>
         </div>
-        <div class="desc-propos left-desc">
-            <p>Go Out ! est né de la passion commune de notre équipe pour les sorties et les événements. Nous avons réalisé qu'il était parfois <span>difficiles de trouver des soirées et des activités intéressantes,</span> surtout lorsque l'on est nouveau dans une ville ou que l'on cherche à diversifier ses expériences. C'est ainsi que nous avons décidé de créer Go Out ! pour faciliter la découverte et l'organisation d'événements.</p>
-        </div>
-    </div>
-    <div class="equipe right" id="equipe">
-        <div class="title">
-            <!-- <img src="" alt="picto"> -->
-            <h1>Équipe</h1>
-        </div>
-        <div class="desc-propos right-desc">
-            <!-- <img src="" alt="Notre équipe"> -->
-            <p>Notre équipe de <span>passionés</span> travaille sans relâche pour prendre Go Out ! plus excitant et plus complet chaque jour. Nous sommes des amateurs de sorties et nous comprenons les besoins et les attentes de notre communauté. Nous mettons tous en oeuvre pour offrir une expérience utilisateur exceptionnelle et pour réponde aux besoin de notre communauté grandissante.</p>
-        </div>
-    </div>
-    <div class="engagements left" id="engagements">
-        <div class="title">
-            <!-- <img src="" alt="picto"> -->
-            <h1>Engagements</h1>
-        </div>
-        <div class="desc-propos left-desc">
-            <p>Chez Go Out ! nous nous engageons à créer une <span>communauté active, engagée et respectueuse</span>. Nous encourageons nos utilisateurs à interagir, à se rencontrer et à partager des moments mémorables. La sécurité de nos utilisateurs est primordiale, c'est pourquoi nous avons mis en place des mesures de protection et de confidentialité pour garantir une expérience sûre et agréable.</p>
-        </div>
-    </div>
-    <div class="partenariats right" id="partenariats">
-        <div class="title">
-            <!-- <img src="" alt="picto"> -->
-            <h1>Parteneriats</h1>
-        </div>
-        <div class="desc-propos right-desc">
-            <p>Nous sommes fier de <span>collaborer avec des bars, des clubs, des restaurants et d'autres acteurs de l'industrie des sorites</span> pour offrir à nos utulisateurs des expériences spéciales et des offres exclusives. Nous cherchons constamment de nouveaux partenaires pour élargir notre gamme d'événements et offrir encore plus de possibilités à notre communauté.</p>
+        <div class="partenariats right" id="partenariats">
+            <div class="title">
+                <h1>Parteneriats</h1>
+                <img src="{{ URL::asset('images/partenariats.png') }}" alt="picto">
+            </div>
+            <div class="desc-propos right-desc">
+                <p>Nous sommes fier de <span>collaborer avec des bars, des clubs, des restaurants et d'autres acteurs de l'industrie des sorites</span> pour offrir à nos utulisateurs des expériences spéciales et des offres exclusives. Nous cherchons constamment de nouveaux partenaires pour élargir notre gamme d'événements et offrir encore plus de possibilités à notre communauté.</p>
+            </div>
         </div>
     </div>
 </x-app-layout>
--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -20,59 +20,61 @@
         </div>
         <a href="#">Foire aux questions</a>
     </div>
-    <div class="card" id="faq-1" onclick="toggleDescription('faq-1')">
-        <h1>Qu'est-ce que "Go Out !" ?</h1>
-        <p class="description">"Go Out ! est une plateforme web et web mobile qui vous permet de trouver et de créer des soirées et des activités selon vos centres d'intérêt. C'est un moyen pratique de découvrir des événements et de rencontrer de nouvelles personnes."</p>
-    </div>
-    <div class="card" id="faq-2" onclick="toggleDescription('faq-2')">
-        <h1>Comment puis-je m'inscrire sur "Go Out !" ?</h1>
-        <p class="description">En construction ...</p>
-    </div>
-    <div class="card" id="faq-3" onclick="toggleDescription('faq-3')">
-        <h1>Comment puis-je trouver des événements sur "Go Out !" ?</h1>
-        <p class="description">En construction ...</p>
-    </div>
-    <div class="card" id="faq-4" onclick="toggleDescription('faq-4')">
-        <h1>Puis-je créer mes propres événements sur "Go Out !" ?</h1>
-        <p class="description">En construction ...</p>
-    </div>
-    <div class="card" id="faq-5" onclick="toggleDescription('faq-5')">
-        <h1>Comment puis-je ajouter des amis sur "Go Out !" ?</h1>
-        <p class="description">En construction ...</p>
-    </div>
-    <div class="card" id="faq-6" onclick="toggleDescription('faq-6')">
-        <h1>Comment puis-je garantir ma sécurité lors des événements sur "Go Out !" ?</h1>
-        <p class="description">En construction ...</p>
-    </div>
-    <div class="card" id="faq-7" onclick="toggleDescription('faq-7')">
-        <h1>Comment puis-je signaler un problème sur "Go Out !" ?</h1>
-        <p class="description">En construction ...</p>
-    </div>
-    <div class="card" id="faq-8" onclick="toggleDescription('faq-8')">
-        <h1>"Go Out !" est-il gratuit à utiliser ?</h1>
-        <p class="description">En construction ...</p>
-    </div>
-    <div class="card" id="faq-9" onclick="toggleDescription('faq-9')">
-        <h1>Comment obtenir des bons plans et des remises exclusives sur "Go Out !" ?</h1>
-        <p class="description">En construction ...</p>
-    </div>
-    <div class="card" id="faq-10" onclick="toggleDescription('faq-10')">
-        <h1>Comment puis-je contacter le support client de "Go out !" ?</h1>
-        <p class="description">En construction ...</p>
+    <div class="all-card">
+        <div class="card" id="faq-1" onclick="toggleDescription('faq-1')">
+            <h1>Qu'est-ce que "Go Out !" ?</h1>
+            <p class="description">"Go Out !" est une plateforme web et web mobile qui vous permet de trouver et de créer des soirées et des activités selon vos centres d'intérêt. C'est un moyen pratique de découvrir des événements et de rencontrer de nouvelles personnes.</p>
+        </div>
+        <div class="card" id="faq-2" onclick="toggleDescription('faq-2')">
+            <h1>Comment puis-je m'inscrire sur "Go Out !" ?</h1>
+            <p class="description">En construction ...</p>
+        </div>
+        <div class="card" id="faq-3" onclick="toggleDescription('faq-3')">
+            <h1>Comment puis-je trouver des événements sur "Go Out !" ?</h1>
+            <p class="description">En construction ...</p>
+        </div>
+        <div class="card" id="faq-4" onclick="toggleDescription('faq-4')">
+            <h1>Puis-je créer mes propres événements sur "Go Out !" ?</h1>
+            <p class="description">En construction ...</p>
+        </div>
+        <div class="card" id="faq-5" onclick="toggleDescription('faq-5')">
+            <h1>Comment puis-je ajouter des amis sur "Go Out !" ?</h1>
+            <p class="description">En construction ...</p>
+        </div>
+        <div class="card" id="faq-6" onclick="toggleDescription('faq-6')">
+            <h1>Comment puis-je garantir ma sécurité lors des événements sur "Go Out !" ?</h1>
+            <p class="description">En construction ...</p>
+        </div>
+        <div class="card" id="faq-7" onclick="toggleDescription('faq-7')">
+            <h1>Comment puis-je signaler un problème sur "Go Out !" ?</h1>
+            <p class="description">En construction ...</p>
+        </div>
+        <div class="card" id="faq-8" onclick="toggleDescription('faq-8')">
+            <h1>"Go Out !" est-il gratuit à utiliser ?</h1>
+            <p class="description">En construction ...</p>
+        </div>
+        <div class="card" id="faq-9" onclick="toggleDescription('faq-9')">
+            <h1>Comment obtenir des bons plans et des remises exclusives sur "Go Out !" ?</h1>
+            <p class="description">En construction ...</p>
+        </div>
+        <div class="card" id="faq-10" onclick="toggleDescription('faq-10')">
+            <h1>Comment puis-je contacter le support client de "Go out !" ?</h1>
+            <p class="description">En construction ...</p>
+        </div>
     </div>
     <div class="container">
-        <h1>Contactez-nous</h1>
+        <h1>Formulaire de contact</h1>
         <form action="" method="POST" id="contact-form">
             <div class="form-field">
                 <div class="row">
                     <div class="e">
                         <label for="fullname">Nom</label>
 
-                        <input type="text" class="fullname" name="fullname" placeholder="Nom" required autofocus>
+                        <input type="text" class="fullname" name="fullname" required autofocus>
                     </div>
                     <div class="e">
                         <label for="name">Prénom</label>
-                        <input type="text" class="name" name="name" placeholder="Prénom" required autofocus>
+                        <input type="text" class="name" name="name" required autofocus>
 
                     </div>
                 </div>
@@ -80,15 +82,15 @@
             </div>
             <div class="form-field">
                 <label for="email">Email</label>
-                <input type="email" class="email" name="email" placeholder="Email" required>
+                <input type="email" class="email" name="email" required>
             </div>
             <div class="form-field">
                 <label for="Sujet">Sujet</label>
-                <input type="text" class="Sujet" name="Sujet" placeholder="Sujet" required autofocus>
+                <input type="text" class="Sujet" name="Sujet" required autofocus>
             </div>
             <div class="form-field">
                 <label for="message">Message</label>
-                <textarea type="message" class="message" name="message" placeholder="Entrez votre message ici" required rows="4"></textarea>
+                <textarea type="message" class="message" name="message" required rows="4"></textarea>
             </div>
             <div class="form-field btn">
                 <input type="submit" name="submit" class="submit" value="Envoyer">
